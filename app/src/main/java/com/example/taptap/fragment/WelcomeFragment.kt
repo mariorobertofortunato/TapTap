fc83812deaff6e93325bc533c12dd8820b5ff642
mariorobertofortunato <mariorobertofortunato@gmail.com>
2022-03-23 15:42:12 +0100

    Basic layout and navigation

--- app/src/main/java/com/example/taptap/fragment/WelcomeFragment.kt
+++ app/src/main/java/com/example/taptap/fragment/WelcomeFragment.kt
@@ -1,25 +1,35 @@
 package com.example.taptap.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.taptap.R
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.taptap.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
 
+    private lateinit var binding: FragmentWelcomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+        binding = FragmentWelcomeBinding.inflate(layoutInflater)
+
+        binding.customButton.setOnClickListener {
+            val action = WelcomeFragmentDirections.actionWelcomeFragmentToMainFragment()
+            findNavController().navigate(action)
+        }
+
+
+
+        return binding.root
+
     }
 }
 
