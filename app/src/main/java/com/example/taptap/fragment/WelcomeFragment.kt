9ff3c5a480d008ffbfde8334b34ee04ac607069f
mariorobertofortunato <mariorobertofortunato@gmail.com>
2022-03-26 18:24:20 +0100

    Second row of animated vector letters

--- app/src/main/java/com/example/taptap/fragment/WelcomeFragment.kt
+++ app/src/main/java/com/example/taptap/fragment/WelcomeFragment.kt
@@ -2,13 +2,11 @@ package com.example.taptap.fragment
 
 import android.animation.ObjectAnimator
 import android.animation.PropertyValuesHolder
-import android.animation.ValueAnimator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import androidx.compose.animation.core.infiniteRepeatable
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.taptap.databinding.FragmentWelcomeBinding
@@ -18,29 +16,35 @@ class WelcomeFragment : Fragment() {
     private lateinit var binding: FragmentWelcomeBinding
 
     //Vector Letters declaration
-    lateinit var letterT : ImageView
-    lateinit var letterA : ImageView
-    lateinit var letterP : ImageView
+    private lateinit var letterT1 : ImageView
+    private lateinit var letterA1 : ImageView
+    private lateinit var letterP1 : ImageView
+    private lateinit var letterT2 : ImageView
+    private lateinit var letterA2 : ImageView
+    private lateinit var letterP2 : ImageView
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
 
         binding = FragmentWelcomeBinding.inflate(layoutInflater)
 
         //Vector Letters Init
-        letterT = binding.t
-        letterA = binding.a
-        letterP = binding.p
+        letterT1 = binding.t1
+        letterA1 = binding.a1
+        letterP1 = binding.p1
+        letterT2 = binding.t2
+        letterA2 = binding.a2
+        letterP2 = binding.p2
 
-        //Button anim
+        //Animation (custom btn + letters)
         buttonAnimation()
-        tScaler()
-        aScaler()
-        pScaler()
+        tScaler(letterT1,0)
+        aScaler(letterA1,100)
+        pScaler(letterP1,200)
+        tScaler(letterT2,150)
+        aScaler(letterA2,250)
+        pScaler(letterP2,300)
 
+        // Navigate to MainFrag
         binding.customButton.setOnClickListener {
             val action = WelcomeFragmentDirections.actionWelcomeFragmentToMainFragment()
             findNavController().navigate(action)
@@ -58,37 +62,42 @@ class WelcomeFragment : Fragment() {
         binding.customButton.animate().alpha(1f).translationY(-1f).duration = 1500
     }
 
-    private fun tScaler() {
+    private fun tScaler(letter : ImageView, delay : Long) {
         val scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X, 1.5f)
         val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y, 1.5f)
         val animator = ObjectAnimator.ofPropertyValuesHolder(
-            letterT, scaleX, scaleY)
+            letter, scaleX, scaleY)
         animator.repeatCount = ObjectAnimator.INFINITE
         animator.repeatMode = ObjectAnimator.REVERSE
+        animator.startDelay = delay
         animator.start()
     }
 
-    private fun aScaler() {
+    private fun aScaler(letter : ImageView, delay : Long) {
         val scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X, 1.25f)
         val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y, 1.25f)
         val animator = ObjectAnimator.ofPropertyValuesHolder(
-            letterA, scaleX, scaleY)
+            letter, scaleX, scaleY)
         animator.repeatCount = ObjectAnimator.INFINITE
         animator.repeatMode = ObjectAnimator.REVERSE
-        animator.startDelay = 100
+        animator.startDelay = delay
         animator.start()
     }
 
-    private fun pScaler() {
+    private fun pScaler(letter : ImageView, delay : Long) {
         val scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X, 1.5f)
         val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y, 1.5f)
         val animator = ObjectAnimator.ofPropertyValuesHolder(
-            letterP, scaleX, scaleY)
+            letter, scaleX, scaleY)
         animator.repeatCount = ObjectAnimator.INFINITE
         animator.repeatMode = ObjectAnimator.REVERSE
-        animator.startDelay = 200
+        animator.startDelay = delay
         animator.start()
     }
+
+
+
+
 
 
 }
